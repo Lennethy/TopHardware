30d39d175f11df518d7a9e281e7ec8ef821dec4e
Rodrigo Nunes <rodrigonunes.k2@gmail.com>
2018-06-30 19:36:08 -0300

    Init

---
+++ app/src/main/java/pooa20181/iff/edu/br/tophardware/ActMain.java
@@ -0,0 +1,13 @@
+package pooa20181.iff.edu.br.tophardware;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
